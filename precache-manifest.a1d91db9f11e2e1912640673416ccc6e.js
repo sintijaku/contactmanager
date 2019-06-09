64fadbc991a84cb35b89b05efd33b8b1883a4dbc
sintijaku <49586750+sintijaku@users.noreply.github.com>
2019-06-09 13:46:44 +0300

    Updates

---
+++ precache-manifest.a1d91db9f11e2e1912640673416ccc6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f96037d98126deed72320d2e7cb3138",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "f87d786b837200621517",
+    "url": "/contactmanager/static/css/2.b9dc5a1e.chunk.css"
+  },
+  {
+    "revision": "e4c89e626f864945f4b7",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f87d786b837200621517",
+    "url": "/contactmanager/static/js/2.7d79791d.chunk.js"
+  },
+  {
+    "revision": "e4c89e626f864945f4b7",
+    "url": "/contactmanager/static/js/main.8fce28a3.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);

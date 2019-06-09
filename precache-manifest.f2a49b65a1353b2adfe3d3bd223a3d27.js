905e86713b724b4180b7d4afe5fe8a87549b663a
sintijaku <49586750+sintijaku@users.noreply.github.com>
2019-06-09 14:06:52 +0300

    Updates

--- precache-manifest.f2a49b65a1353b2adfe3d3bd223a3d27.js
+++ precache-manifest.f2a49b65a1353b2adfe3d3bd223a3d27.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6f96037d98126deed72320d2e7cb3138",
+    "revision": "05706fa5f688192d232a29762c2defff",
     "url": "/contactmanager/index.html"
   },
   {
-    "revision": "f87d786b837200621517",
+    "revision": "d5b4a97e307a7e12cab6",
     "url": "/contactmanager/static/css/2.b9dc5a1e.chunk.css"
   },
   {
-    "revision": "e4c89e626f864945f4b7",
+    "revision": "a8f66410014b9ff859ee",
     "url": "/contactmanager/static/css/main.36497136.chunk.css"
   },
   {
-    "revision": "f87d786b837200621517",
-    "url": "/contactmanager/static/js/2.7d79791d.chunk.js"
+    "revision": "d5b4a97e307a7e12cab6",
+    "url": "/contactmanager/static/js/2.edd9a460.chunk.js"
   },
   {
-    "revision": "e4c89e626f864945f4b7",
-    "url": "/contactmanager/static/js/main.8fce28a3.chunk.js"
+    "revision": "a8f66410014b9ff859ee",
+    "url": "/contactmanager/static/js/main.b870573e.chunk.js"
   },
   {
     "revision": "a9b80a0b0e722e0d3664",
